381383bb2a332fe0be3854b98c8d8e07711c9200
Russell Wallace <russell.wallace@gmail.com>
2017-03-31 20:18:01 +0100

    unify

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 'use strict';
 
+function unifyVar(a, b, m) {
+}
+
 // API
 
-function unify(a, b) {
+function unify(a, b, m) {
+	if (a === b) {
+		return true;
+	}
+	if (a.op === 'var') {
+		return unifyVar(a, b, m);
+	}
+	if (b.op === 'var') {
+		return unifyVar(b, a, m);
+	}
+	if (a.op !== b.op) {
+		return false;
+	}
+	if (!a.args) {
+		return a.val === b.val;
+	}
+	if (a.args.length !== b.args.length) {
+		return false;
+	}
+	for (var i = 0; i < a.length; i++) {
+		if (!unify(a.args[i], b.args[i], m)) {
+			return false;
+		}
+	}
+	return true;
 }
 
 exports.unify = unify;
