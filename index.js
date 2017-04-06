091c3a66d66b149842eece028facbe59b811e21a
Russell Wallace <russell.wallace@gmail.com>
2017-04-06 05:59:42 +0100

    format

--- index.js
+++ index.js
@@ -1,72 +1,56 @@
-'use strict';
+'use strict'
 
 function unifyVar(a, b, m) {
-	if (m.has(a)) {
-		return unify(m.get(a), b, m);
-	}
-	if (m.has(b)) {
-		return unify(a, m.get(b), m);
-	}
-	if (occurs(a, b, m)) {
-		return null;
-	}
-	m.set(a, b);
-	return m;
+	if (m.has(a))
+		return unify(m.get(a), b, m)
+	if (m.has(b))
+		return unify(a, m.get(b), m)
+	if (occurs(a, b, m))
+		return null
+	m.set(a, b)
+	return m
 }
 
 // Exports
 
 function occurs(a, b, m) {
-	if (a === b) {
-		return true;
-	}
-	if (m.has(b)) {
-		return occurs(a, m.get(b), m);
-	}
-	if (!b.args) {
-		return false;
-	}
-	for (var x of b.args) {
-		if (occurs(a, x, m)) {
-			return true;
-		}
-	}
+	if (a === b)
+		return true
+	if (m.has(b))
+		return occurs(a, m.get(b), m)
+	if (!b.args)
+		return false
+	for (var x of b.args)
+		if (occurs(a, x, m))
+			return true
 }
 
 function unify(a, b, m) {
-	m = m || new Map();
-	if (a === b) {
-		return m;
-	}
-	if (a.op === 'var') {
-		return unifyVar(a, b, m);
-	}
-	if (b.op === 'var') {
-		return unifyVar(b, a, m);
-	}
-	if (a.op !== b.op) {
-		return null;
-	}
+	m = m || new Map()
+	if (a === b)
+		return m
+	if (a.op === 'var')
+		return unifyVar(a, b, m)
+	if (b.op === 'var')
+		return unifyVar(b, a, m)
+	if (a.op !== b.op)
+		return null
 	switch (a.op) {
 	case 'call':
-		if (a.fun !== b.fun) {
-			return null;
-		}
-		break;
+		if (a.fun !== b.fun)
+			return null
+		break
 	case 'const':
-		return a.val === b.val;
+		return a.val === b.val
 	}
-	if (!a.args) {
-		return m;
-	}
-	if (a.args.length !== b.args.length) {
-		return null;
-	}
-	for (var i = 0; i < a.args.length && m; i++) {
-		m = unify(a.args[i], b.args[i], m);
-	}
-	return m;
+	if (!a.args)
+		return m
+	if (a.args.length !== b.args.length)
+		return null
+	for (var i = 0; i < a.args.length && m; i++)
+		m = unify(a.args[i], b.args[i], m)
+	return m
 }
 
-exports.occurs = occurs;
-exports.unify = unify;
+exports.occurs = occurs
+exports.unify = unify
